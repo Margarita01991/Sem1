fd6b8b5cad9a11a305cbd95cfc813656b49783d6
Margarita <margarita27011991@gmail.com>
2022-08-07 22:25:42 +0300

    Add task0

---
+++ task0/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу которая на вход принимает число и выдает его квадрат
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number; 
+Console.Write(square); 
