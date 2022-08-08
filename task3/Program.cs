41b11c2185ac648faf5af61d588cd9ff65f2ef47
Margarita <margarita27011991@gmail.com>
2022-08-08 16:18:36 +0300

    add task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,19 +1,35 @@
 ﻿// Программа которая будет выдавать название дня недели по заданному номеру
-Console.Write("Введите число");
-string weekday = Console.ReadLine();
-if(weekday() == "1")
+Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
 {
-    Console.WriteLine("Monday");
+    Console.WriteLine("Понедельник");
 }
-     else
+else if(day == 2)
 {
-    Console.Write("Привет, ");
+    Console.WriteLine("Вторник");
 }
-//Console.Write (2) = Console.ReadLine("Monday");
-//Console.Write (3) = Console.ReadLine("Tuesday");
-//Console.Write (4) = Console.ReadLine("Цeвnуsday");
-//Console.Write (5) = Console.ReadLine("Thursday");
-//Console.Write (6) = Console.ReadLine("Friday");
-//Console.Write (7) = Console.ReadLine("Saturday");
-//Console.Write (1) = Console.ReadLine("Saturday");
-
+else if(day == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(day == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(day == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(day == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(day == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else if(day > 7)
+{
+    Console.WriteLine("Нет такого дня недели");
+}
