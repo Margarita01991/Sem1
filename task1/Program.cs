e8b99b63e4431a02bc7a12efb89ede9239ddec81
Margarita <margarita27011991@gmail.com>
2022-08-08 15:32:57 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Программа которая на вход принимает два числа и проверяет является ли первое число квадратом второго
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int squareB = numberB * numberB;
+
+    if (numberA == squareB)
+{
+    Console.WriteLine ("Да");
+}
+    else
+{    
+    Console.WriteLine ("Нет");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа которая будет выдавать название дня недели по заданному номеру
+Console.Write("Введите число");
+string weekday = Console.ReadLine();
+if(weekday() == "1")
+{
+    Console.WriteLine("Monday");
+}
+     else
+{
+    Console.Write("Привет, ");
+}
+//Console.Write (2) = Console.ReadLine("Monday");
+//Console.Write (3) = Console.ReadLine("Tuesday");
+//Console.Write (4) = Console.ReadLine("Цeвnуsday");
+//Console.Write (5) = Console.ReadLine("Thursday");
+//Console.Write (6) = Console.ReadLine("Friday");
+//Console.Write (7) = Console.ReadLine("Saturday");
+//Console.Write (1) = Console.ReadLine("Saturday");
+
