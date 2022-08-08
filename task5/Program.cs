6cc388b4bd62b2253797cb6b9e4a0a29e956c071
Margarita <margarita27011991@gmail.com>
2022-08-08 18:23:23 +0300

    add task5

---
+++ task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа на вход принимает одно число N на выходе показывает все целые числа в промежутке от -N до N
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int L = -N;
+
+while (L <= N)
+{
+    Console.WriteLine(L);
+    L++;
+}
+
+
