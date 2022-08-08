1b00ead059497c91216574ecb49ab5e55ac852cc
Margarita <margarita27011991@gmail.com>
2022-08-08 18:37:10 +0300

    add task7

---
+++ task7/Program.cs
@@ -0,0 +1,5 @@
+﻿// программа которая принимает на вход трехзначное число а на выходе показывает последнюю цифру этого числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int N = number%10;
+Console.WriteLine(N);
